7e5b14c8cac17928acba9954f2c82b9f857bb3bb
Umair-230104 <144212017+Umair-230104@users.noreply.github.com>
2023-11-09 19:02:49 +0100

    Update AFuelCar.java

--- SP2/src/AFuelCar.java
+++ SP2/src/AFuelCar.java
@@ -1,7 +1,6 @@
 public abstract class AFuelCar extends ACar {
     private int kmPrLitre;
 
-    // Under her lavede programmet det selv
     public AFuelCar(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre) {
         super(registrationNumber, make, model, numberOfDoors);
         this.kmPrLitre = kmPrLitre;
